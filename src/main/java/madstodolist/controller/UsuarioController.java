993aec61bd7c037f78993f537418bde2e7f371c6
F 3 R K H 4 N <133190285+Ferkhan@users.noreply.github.com>
2025-06-10 01:34:37 -0500

    feat(navbar-aut): agregar barra menú para usuarios autenticados

---
+++ src/main/java/madstodolist/controller/UsuarioController.java
@@ -0,0 +1,29 @@
+package madstodolist.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import madstodolist.dto.UsuarioData;
+import madstodolist.service.UsuarioService;
+
+@Controller
+public class UsuarioController {
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping("/cuenta")
+    public String cuentaUsuario(Model model, HttpSession session) {
+        Long idUsuario = (Long) session.getAttribute("idUsuarioLogeado");
+        if (idUsuario == null) {
+            return "redirect:/login";
+        }
+        UsuarioData usuario = usuarioService.findById(idUsuario);
+        model.addAttribute("usuario", usuario);
+        return "cuentaUsuario";
+    }
+}
